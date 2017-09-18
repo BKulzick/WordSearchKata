91b4e8c7d5ff2d13f5e313fa7b995404bf202034
BKulzick <BryanKulczycki@gmail.com>
2017-09-18 10:55:58 -0400

    Cleaned up Tests with Constants

--- WordSearchService/src/test/java/com/wordSearch/utils/WordFinderTest.java
+++ WordSearchService/src/test/java/com/wordSearch/utils/WordFinderTest.java
@@ -9,24 +9,29 @@ import com.wordSearch.model.WordFinderAnswer;
 
 public class WordFinderTest {
 
+	private static final String STRING_HAS_BOY_AT_5 = "addadboydcefs";
+	private static final String NOT_FOUND = "abcyefg";
+	private static final String STRING_HAS_BOY_AT_4 = "abcboyefg";
+	private static final String BOY = "boy";
+
 	@Test
 	public void whenGivenLettersWithWordEnsureGivenWordFound() {
 		WordFinder finder = new WordFinder();
-		WordFinderAnswer answer = finder.searchFor("boy","abcboyefg");
+		WordFinderAnswer answer = finder.searchFor(BOY,STRING_HAS_BOY_AT_4);
 		assertTrue(answer.getIsWordFound());
 	}
 	
 	@Test
 	public void whenGivenLettersWithNoWordEnsureGivenWordNotFound() {
 		WordFinder finder = new WordFinder();
-		WordFinderAnswer answer = finder.searchFor("boy","abcyefg");
+		WordFinderAnswer answer = finder.searchFor(BOY,NOT_FOUND);
 		assertFalse(answer.getIsWordFound());
 	}
 	
 	@Test
 	public void whenWordIsFoundEnsureXCoordinateInLineReturns() {
 		WordFinder finder = new WordFinder();
-		WordFinderAnswer answer = finder.searchFor("boy","addadboydcefs");
+		WordFinderAnswer answer = finder.searchFor(BOY,STRING_HAS_BOY_AT_5);
 		assertTrue(answer.getIsWordFound());
 		Assert.assertEquals(answer.getXLocation(),(Integer)5);
 	}

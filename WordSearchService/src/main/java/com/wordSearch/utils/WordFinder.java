177f387a61e98aad9e2679546697e34a4b193531
BKulzick <BryanKulczycki@gmail.com>
2017-09-18 12:51:39 -0400

    Returns x Location of Word Found, with one occurance

--- WordSearchService/src/main/java/com/wordSearch/utils/WordFinder.java
+++ WordSearchService/src/main/java/com/wordSearch/utils/WordFinder.java
@@ -7,10 +7,15 @@ public class WordFinder {
 	public WordFinderAnswer searchFor(String word, String puzzleLine) {
 		if (puzzleLine.contains(word)) { 
 			WordFinderAnswer answer =new WordFinderAnswer(true);
-			answer.setXLocation(5);
+			Integer location  = findLocationOfWordInPuzzle(word,puzzleLine);
+			answer.setXLocation(location);
 			return answer; 
 			}
 		return new WordFinderAnswer(false);
+	}
+
+	private Integer findLocationOfWordInPuzzle(String word, String puzzleLine) {
+		return puzzleLine.indexOf(word.substring(0, 1));
 	}
 
 }
--- WordSearchService/src/test/java/com/wordSearch/utils/WordFinderTest.java
+++ WordSearchService/src/test/java/com/wordSearch/utils/WordFinderTest.java
@@ -9,9 +9,11 @@ import com.wordSearch.model.WordFinderAnswer;
 
 public class WordFinderTest {
 
+	private static final Integer FOURTH_SPOT = 4;
+	private static final Integer FIFTH_SPOT = 5;
 	private static final String STRING_HAS_BOY_AT_5 = "addadboydcefs";
 	private static final String NOT_FOUND = "abcyefg";
-	private static final String STRING_HAS_BOY_AT_4 = "abcboyefg";
+	private static final String STRING_HAS_BOY_AT_4 = "gadcboyefg";
 	private static final String BOY = "boy";
 
 	@Test
@@ -33,7 +35,15 @@ public class WordFinderTest {
 		WordFinder finder = new WordFinder();
 		WordFinderAnswer answer = finder.searchFor(BOY,STRING_HAS_BOY_AT_5);
 		assertTrue(answer.getIsWordFound());
-		Assert.assertEquals(answer.getXLocation(),(Integer)5);
+		Assert.assertEquals(FIFTH_SPOT, answer.getXLocation());
 	}
+	@Test
+	public void whenWordIsFoundEnsureXCoordinateInLineReturnsAtSpotFour() {
+		WordFinder finder = new WordFinder();
+		WordFinderAnswer answer = finder.searchFor(BOY,STRING_HAS_BOY_AT_4);
+		assertTrue(answer.getIsWordFound());
+		Assert.assertEquals(FOURTH_SPOT,answer.getXLocation());
+	}	
+	
 	
 }
